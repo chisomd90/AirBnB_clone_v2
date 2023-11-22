de34ece0d2c16314377c75d33f7cbb60c79c02be
Onyenze Chetachi <chetaGod4life@gmail.com>
2023-11-22 22:45:28 +0100

    updated user to database storage

--- models/place.py
+++ models/place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
 
 
 class Place(BaseModel):
